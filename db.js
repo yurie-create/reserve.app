d6934bbb2e32b02a4ec3518841c56a6e234838be
yurie <yurie6312@gmail.com>
2026-04-24 10:17:30 +0900

    use railway volume db

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./reserve.db");
+const dbPath =
+  process.env.NODE_ENV === "production"
+    ? "/data/reserve.db"
+    : "reserve.db";
 
+const db = new sqlite3.Database(dbPath);
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS reservations (
