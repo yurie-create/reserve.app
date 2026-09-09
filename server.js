8e687291caf2563e129a4a7b646430f454276925
yurie <yurie6312@gmail.com>
2026-09-09 14:05:54 +0900

    Fix XSS in embedded data and dynamic views

--- server.js
+++ server.js
@@ -215,8 +215,24 @@ function getCourseLabel(course) {
   return COURSE_LABELS[course] || course;
 }
 
+function safeJsonForHtml(value) {
+  const json = JSON.stringify(value);
+
+  if (json === undefined) {
+    return 'null';
+  }
+
+  return json
+    .replace(/</g, '\\u003c')
+    .replace(/>/g, '\\u003e')
+    .replace(/&/g, '\\u0026')
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029');
+}
+
 app.locals.formatPlan = getPlanLabel;
 app.locals.formatCourse = getCourseLabel;
+app.locals.safeJsonForHtml = safeJsonForHtml;
 
 function requireAdmin(req, res, next) {
   if (!req.session.isAdmin) {
